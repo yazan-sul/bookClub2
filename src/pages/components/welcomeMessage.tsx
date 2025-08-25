e23f53657092586088c057669ce6604159f34b0d
yazan-sul <yazan-sul@gmail.com>
2025-08-25 12:34:25 +0300

    new repo

---
+++ src/pages/components/welcomeMessage.tsx
@@ -0,0 +1,9 @@
+
+export default function WelcomeMessage({name} :{ name : string}) {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl py-2">Welcome back, {name} 👋</h1>
+      <p className="text-sm text-slate-700 ">What would you like to read today?</p>
+    </div>
+  );
+}
