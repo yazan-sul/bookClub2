286112644cf4db5ce1342cf544f065b1952765bc
yazan-sul <yazan5sulaiman@gmail.com>
2025-09-04 13:26:14 +0300

    with last commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,44 +3,93 @@ import HeaderBar from "@/components/core/headerBar";
 import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import App from "next/app";
 import { Toaster } from "react-hot-toast";
 import { PagesTopLoader } from "nextjs-toploader/pages";
+import { parse } from "cookie";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+  authData,
+}: AppProps & {
+  authData: { userId: string | null; username: string | null };
+}) {
   return (
-    <AuthProvider>
+    <AuthProvider
+      initialUserId={authData.userId}
+      initialUsername={authData.username}
+    >
       <div className="min-h-screen flex flex-col">
         <HeaderBar />
         <main className="flex-grow">
-          <PagesTopLoader
-            color="#29D" // Color of the loading bar
-            height={4} // Height in px
-            speed={200} // Animation speed (lower is faster)
-            shadow="0 0 10px #29D, 0 0 5px #29D" // Optional shadow
-            showSpinner={false}
-          />
+          <PagesTopLoaderWrapper />
           <Component {...pageProps} />
-          <Toaster
-            position="bottom-center"
-            reverseOrder={false}
-            toastOptions={{
-              success: {
-                style: {
-                  background: "green",
-                  color: "white",
-                },
-              },
-              error: {
-                style: {
-                  background: "red",
-                  color: "white",
-                },
-              },
-            }}
-          />
+          <ToastWrapper />
         </main>
         <Footer />
       </div>
     </AuthProvider>
   );
 }
+MyApp.getInitialProps = async (appContext: any) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const req = appContext.ctx.req;
+  let authData: { userId: string | null; username: string | null } = {
+    userId: null,
+    username: null,
+  };
+
+  if (req) {
+    const cookies = req.headers.cookie ? parse(req.headers.cookie) : {};
+    authData.userId = cookies.user_id || null;
+    authData.username = cookies.username || null;
+  }
+
+  return {
+    ...appProps,
+    authData,
+  };
+};
+const ToastWrapper = () => {
+  return (
+    <Toaster
+      position="bottom-center"
+      reverseOrder={false}
+      toastOptions={{
+        success: {
+          style: {
+            background: "green",
+            color: "white",
+          },
+        },
+        error: {
+          style: {
+            background: "red",
+            color: "white",
+          },
+        },
+      }}
+    />
+  );
+};
+const PagesTopLoaderWrapper = () => {
+  return (
+    <PagesTopLoader
+      color="#2299DD"
+      initialPosition={0.08}
+      crawlSpeed={200}
+      height={3}
+      crawl={true}
+      showSpinner={true}
+      easing="ease"
+      speed={200}
+      shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+      template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+      zIndex={1600}
+      showAtBottom={false}
+    />
+  );
+};
